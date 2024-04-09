c285d446ec53c5f4ffba9c89e53f518050aaae39
stevenlittnyu <75270947+stevenlittnyu@users.noreply.github.com>
2024-04-09 11:43:11 -0400

    added 3rd led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,11 +1,12 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int ledPin3 = 15;
 
 // here's a change
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  
+  pinMode(ledPin3, OUTPUT);
 }
 
 void loop() {
